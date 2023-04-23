52e6e984b36948d3518c5e0fcdc1590d8c037dae
Matheus Passos <mpassos01@gmail.com>
2023-04-23 15:44:20 -0300

    Exercício capitulo 9

--- capitulo_8_introducao_objetos_produto/src/entities/Product.java
+++ capitulo_8_introducao_objetos_produto/src/entities/Product.java
@@ -2,10 +2,15 @@ package entities;
 
 public class Product {
 
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 	
+	
+	
+	public Product() {
+	}
+
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
@@ -28,4 +33,6 @@ public class Product {
 		return name + ", $" + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
 				+ String.format("%.2f", totalValueInStock());
 	}
+	
+	
 }
